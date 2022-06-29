72b6218368ce07df91c94b4a0ee2d47f38e0e9f9
yuseons <yusnseo@gmail.com>
2022-06-29 22:48:05 +0900

    update

--- SpringBoot_ShoppingMall/shopping/src/main/resources/static/js/cart.js
+++
@@ -1,9 +0,0 @@
-function addCart(param){
-	return fetch('/cart/create',{
-        method: 'post',
-        body: JSON.stringify(param),
-        headers: {'Content-Type': "application/json; charset=utf-8"}
-        }) //post방식은 method,body,headers를 써줌
-        .then(response => response.text())
-        .catch(console.log);
-}
